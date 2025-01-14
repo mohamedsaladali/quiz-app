402825215458d1ed4b5e00cd8d3f4980a9aeaffb
Mohamed Salad <maxamedsalad946@gmail.com>
2025-01-14 12:22:41 +0300

    Added new project

---
+++ lib/data/questions.dart
@@ -0,0 +1,58 @@
+import 'package:quiz_app/models/quiz_question.dart';
+
+const questions = [
+  QuizQuestion(
+    'Sanadkee  ayay Soomaaliya xornimada qaadatay?',
+    [
+      '1960',
+      '1950',
+      '1970',
+      '1980',
+    ],
+  ),
+  QuizQuestion(
+    'Labadee gobol ayaa midoobay si loo sameeyo Jamhuuriyadda Soomaaliya sanadkii 1960?',
+    [
+      'Woqooyi  iyo koofur',
+      'Bari iyo waqooyi',
+      'Koofur iyo galbeed',
+      'midna',
+    ],
+  ),
+  QuizQuestion(
+    'Kumuu ahaa madaxweynihii ugu horreeyay ee Soomaaliya?',
+    [
+      'Aadan Cabdulle Cismaan',
+      'Maxamed Siyaad Barre',
+      'Xasan Sheekh Maxamuud',
+      'Cabdirashiid Cali Sharmaarke',
+    ],
+  ),
+  QuizQuestion(
+    'Waa maxay magaca boqortooyadii badda ee hore ee Soomaaliya?',
+    [
+      'Saldanadda Ajuuraan',
+      'Boqortooyada Cusmaaniyiinta',
+      'Saldanadda Mamluk',
+      'Saldanadda Zanzibaar',
+    ],
+  ),
+  QuizQuestion(
+    'Luuqaddee ayaa si rasmi ah loogu aqoonsaday luuqadda qaran ee Soomaaliya sanadkii 1972?',
+    [
+      'Af-Soomaali',
+      'Carabi',
+      'Ingiriisi',
+      'Talyaani',
+    ],
+  ),
+  QuizQuestion(
+    'Sanadkee ayuu dagaalkii sokeeye ee Soomaaliya bilowday?',
+    [
+      '1991',
+      '1988',
+      '2000',
+      '1995',
+    ],
+  ),
+];
